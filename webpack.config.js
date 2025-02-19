42bfbd3a9e9e1b95189fc4a1fe0f6217a4ad782c
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-02-18 21:34:09 -0300

    Incluindo Babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,36 @@
-const path = require("path") // Importa o módulo 'path' do Node.js, que fornece utilitários para trabalhar com caminhos de arquivos e diretórios.
-const HTMLWebpackPlugin = require("html-webpack-plugin")
-module.exports = { // Exporta um objeto que contém a configuração do Webpack.
-    entry: path.resolve(__dirname, "src", "js", "index.js"), // Define o ponto de entrada da aplicação, que é o arquivo 'index.js' localizado na pasta 'src/js'. __dirname é uma variável que representa o diretório atual do arquivo.
-    
-    output: { // Define as configurações de saída do Webpack.
-        filename: "main.js", // Nome do arquivo de saída que será gerado após o processo de bundling.
-        path: path.resolve(__dirname, "dist"), // Define o diretório onde o arquivo de saída será salvo, que é a pasta 'dist' no diretório atual.
-    },
-    
-    mode: "development", // Define o modo de operação do Webpack. 'development' ativa várias funcionalidades úteis para desenvolvimento, como a geração de mapas de origem (source maps).
-    plugins: [new HTMLWebpackPlugin()],
+const path = require("path"); // Importa o módulo 'path' do Node.js, que fornece utilitários para trabalhar com caminhos de arquivos e diretórios.
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  // Exporta um objeto que contém a configuração do Webpack.
+  entry: path.resolve(__dirname, "src", "js", "index.js"), // Define o ponto de entrada da aplicação, que é o arquivo 'index.js' localizado na pasta 'src/js'. __dirname é uma variável que representa o diretório atual do arquivo.
 
-    module:{
-        rules:[{
-            test:/\.Css$/i, 
-            use: ["style-loader", "css-loader"],
-            exclude: "/node_modules",
-        }]
-    }
-}
+  output: {
+    // Define as configurações de saída do Webpack.
+    filename: "main.js", // Nome do arquivo de saída que será gerado após o processo de bundling.
+    path: path.resolve(__dirname, "dist"), // Define o diretório onde o arquivo de saída será salvo, que é a pasta 'dist' no diretório atual.
+  },
+
+  mode: "development", // Define o modo de operação do Webpack. 'development' ativa várias funcionalidades úteis para desenvolvimento, como a geração de mapas de origem (source maps).
+  plugins: [new HTMLWebpackPlugin()],
+
+  module: {
+    // CSS
+    rules: [
+      {
+        test: /\.Css$/i,
+        use: ["style-loader", "css-loader"],
+        exclude: "/node_modules",
+      },
+      {
+        test: /\.js$/i,
+        exclude: "/node_modueles",
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
+};
